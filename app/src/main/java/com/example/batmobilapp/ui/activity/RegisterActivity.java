1e49e75020faa92fc83aec97dfdf5f6acbfa00f7
Sümeyye Sarıçam <sumeyye.saricam@appcent.mobi>
2019-09-28 16:32:08 +0300

    register fix.

--- app/src/main/java/com/example/batmobilapp/ui/activity/RegisterActivity.java
+++ app/src/main/java/com/example/batmobilapp/ui/activity/RegisterActivity.java
@@ -6,8 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import com.example.batmobilapp.MainPage;
 import com.example.batmobilapp.R;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -21,7 +19,7 @@ public class RegisterActivity extends AppCompatActivity {
         signUp_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(RegisterActivity.this, MainPage.class));
+                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
             }
         });
     }
