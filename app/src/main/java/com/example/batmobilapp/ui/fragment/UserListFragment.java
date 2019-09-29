c73688ff1ec2c8871d31a198af38f43974193168
shnsenanur <48882047+shnsenanur@users.noreply.github.com>
2019-09-29 15:06:13 +0300

    user_category_list deleted

--- app/src/main/java/com/example/batmobilapp/ui/fragment/UserListFragment.java
+++ app/src/main/java/com/example/batmobilapp/ui/fragment/UserListFragment.java
@@ -1,28 +1,74 @@
 package com.example.batmobilapp.ui.fragment;
 
-import android.content.Context;
-import android.net.Uri;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.graphics.drawable.Drawable;
 import com.example.batmobilapp.R;
 
 public class UserListFragment extends Fragment {
+    private int id;
+    private String title;
+    private Drawable icon;
+    private String description;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public UserListFragment() {
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_user_list, container, false);
+    public UserListFragment(int id, String title, Drawable icon, String description)
+    {
+        this.id = id;
+        this.title = title;
+        this.icon = icon;
+        this.description = description;
     }
 
-}
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public Drawable getIcon()
+    {
+        return icon;
+    }
+
+    public void setIcon(Drawable icon)
+    {
+        this.icon = icon;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }}
+
+
+
+
+
+
+
