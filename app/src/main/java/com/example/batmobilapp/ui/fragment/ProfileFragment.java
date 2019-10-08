e00d45ed0350d54649c1468a5018cff81561d75b
shnsenanur <48882047+shnsenanur@users.noreply.github.com>
2019-10-08 13:30:02 +0300

    rateBar and infoTable added

--- app/src/main/java/com/example/batmobilapp/ui/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/batmobilapp/ui/fragment/ProfileFragment.java
@@ -12,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RatingBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -40,12 +41,16 @@ public class ProfileFragment extends Fragment implements LocationListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
         btnAcil = view.findViewById(R.id.btnAcil);
+
         return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        RatingBar ratingBar2;
+        ratingBar2 = view.findViewById(R.id.ratingBar2);
+        ratingBar2.setNumStars(5);
         getActivity().setTitle("Profil");
         Dexter.withActivity(getActivity())
                 .withPermissions(
--- app/src/main/java/com/example/batmobilapp/ui/fragment/UserFragment.java
+++ app/src/main/java/com/example/batmobilapp/ui/fragment/UserFragment.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TableLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,7 +20,12 @@ import com.example.batmobilapp.data.model.User;
 
 public class UserFragment extends Fragment {
 
+
     TextView tvUsername, tvAge;
+    RatingBar ratingBar;
+    Button switchBtn;
+
+    private boolean table_flg = false;
 
     public static UserFragment newInstance(User user) {
         Bundle args = new Bundle();
@@ -31,12 +40,40 @@ public class UserFragment extends Fragment {
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_user, container, false);
     }
+    public void collapseTable(View view) {
+        TableLayout table = table.findViewById(table_flg);
+        Button switchBtn = switchBtn.findViewById(switchBtn);
+
+        // setColumnCollapsed(int columnIndex, boolean isCollapsed)
+        table.setColumnCollapsed(1, table_flg);
+        table.setColumnCollapsed(2, table_flg);
+
+        if (table_flg) {
+            // Close
+            table_flg = false;
+            switchBtn.setText("Show Detail");
+        } else {
+            // Open
+            table_flg = true;
+            switchBtn.setText("Hide Detail");
+        }
+
+    }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         tvUsername = view.findViewById(R.id.tvUsername);
         tvAge = view.findViewById(R.id.tvAge);
+        ratingBar = view.findViewById(R.id.ratingBar);
+        ratingBar.setNumStars(5);
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+
+            }
+        });
 
         Bundle extras = getArguments();
         if (extras != null) {
